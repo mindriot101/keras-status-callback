9931684655338904896a894363beae4ab72c6185
Simon Walker <s.r.walker101@googlemail.com>
2018-06-12 23:57:28 +0100

    Set up package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, division, absolute_import
+from setuptools import setup, find_packages
+
+setup(
+        name='keras-status-callback',
+        description='Keras callback for writing to a database',
+        version='0.0.1',
+        author='Simon Walker',
+        author_email='s.r.walker101@googlemail.com',
+        packages=find_packages(),
+        )
